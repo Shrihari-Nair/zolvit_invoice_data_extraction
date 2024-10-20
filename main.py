db01c3f74bb04b61100f0e6183635b57974d18bb
Shrihari-Nair <shriharinair@htic.iitm.ac.in>
2024-10-20 22:10:57 +0530

    check for compute time

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from extract import extract_invoice_data
 import os
 import glob
+import time
 
 def main():
+    start_time = time.time()
     dir_path = "./data/Jan to Mar"
     pdf_pattern = os.path.join(dir_path, '*.pdf')
     # Get a list of all PDF files in the directory
@@ -11,6 +13,9 @@ def main():
     for pdf_file in pdf_files:
         print(pdf_file.split("/")[-1])
         extract_invoice_data(pdf_file)
-
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution Time: {execution_time} seconds")
+    
 if __name__ == "__main__":
     main()
